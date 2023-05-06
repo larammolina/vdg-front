a4aae90d69a160da530cb44e7807ee8cb3f67815
Macarena Lara Molina <molina.macarenalara@gmail.com>
2023-05-06 14:19:40 -0300

    agrego titulos

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Viernes de Garage</h1>
+      <h1>5 de Copas</h1>
     </>
   )
 }
