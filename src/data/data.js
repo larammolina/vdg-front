71d243ac3442d223ad9a81fbb47b8c36d6398838
Iara Baya <iarabayav@gmail.com>
2023-06-16 17:06:52 -0300

    feat: cards en pantalla servicios que redireccionen

---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+const torta1 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+]
+
+const torta2 = [
+  { name: 'Group A', value: 900 },
+  { name: 'Group B', value: 300 },
+]
+
+const torta3 = [
+  { name: 'Group A', value: 500 },
+  { name: 'Group B', value: 300 },
+]
+
+const servicios = [
+  {
+    index: "a",
+    path: "contratacion-prod",
+    title: "Contratación Producto",
+    servicio: "VTAS3ROS_CONTRATACION_PRODUCTO",
+  },
+  {
+    index: "b",
+    path: "contratacion-cli",
+    title: "Contratación Cliente",
+    servicio: "VTAS3ROS_CONTRATACION_CLIENTE",
+  },
+  {
+    index: "c",
+    path: "evaluacion",
+    title: "Evaluación Crediticia",
+    servicio: "VTAS3ROS_EVALUACION_RIESGO_COMPLETA",
+  },
+];
+
+
+export {
+	torta1,
+	torta2,
+	torta3,
+	servicios
+}
